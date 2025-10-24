a75afb8ebf1df6ca908abba28ea418a5b872c8df
Ann Mariya Tony <annmariyatony2028@cs.sjcetpalai.ac.in>
2025-10-24 12:29:22 +0530

    Update Order.java

--- src/com/canteen/model/Order.java
+++ src/com/canteen/model/Order.java
@@ -4,24 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Order {
+    private List<OrderItem> items;
 
-    private List<OrderItem> items = new ArrayList<>();
+    public Order() {
+        this.items = new ArrayList<>();
+    }
 
     public void addItem(OrderItem item) {
         this.items.add(item);
     }
 
-    
+    public List<OrderItem> getItems() {
+        return items;
+    }
+
     public double getSubtotal() {
         double subtotal = 0.0;
         for (OrderItem item : items) {
-            
-            subtotal += item.getTotal();
+            subtotal += item.getTotalPrice();
         }
         return subtotal;
-    }
-
-    public List<OrderItem> getItems() {
-        return items;
     }
 }
