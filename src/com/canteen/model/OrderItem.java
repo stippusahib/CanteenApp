97c7f3d688198fbc8e0151f2551230a50bfaa605
Ann Mariya Tony <annmariyatony2028@cs.sjcetpalai.ac.in>
2025-10-24 11:44:44 +0530

    Update OrderItem.java

--- src/com/canteen/model/OrderItem.java
+++ src/com/canteen/model/OrderItem.java
@@ -1,1 +1,21 @@
+package com.canteen.model;
 
+public class OrderItem {
+    private String name;
+    private double price;
+    private int quantity;
+
+    public OrderItem(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public double getTotal() {
+        return price * quantity;
+    }
+
+    public String getName() { return name; }
+    public double getPrice() { return price; }
+    public int getQuantity() { return quantity; }
+}
