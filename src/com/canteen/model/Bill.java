993771b1a25849b19edd2057e7266ab1ea460864
Ann Mariya Tony <annmariyatony2028@cs.sjcetpalai.ac.in>
2025-10-24 11:41:52 +0530

    Update Bill.java

--- src/com/canteen/model/Bill.java
+++ src/com/canteen/model/Bill.java
@@ -1,1 +1,44 @@
-q
+package com.canteen.model;
+
+public class Bill {
+    private static final double TAX_RATE = 0.05;
+    protected Order order;
+
+    public Bill(Order order) {
+        this.order = order;
+    }
+
+    public double getTax() {
+        return order.getSubtotal() * TAX_RATE;
+    }
+
+    public double getTotal() {
+        return order.getSubtotal() + getTax();
+    }
+
+    
+    public String generateBillText() {
+        String bill = "";
+        
+        bill = bill + "===== CANTEEN BILL =====\n";
+        bill = bill + "Item          Qty  Price  Total\n";
+        bill = bill + "--------------------------------\n";
+
+        // Loop through items 
+        for (OrderItem item : order.getItems()) {
+            bill = bill + item.getName() + "  " 
+                        + item.getQuantity() + "   " 
+                        + item.getPrice() + "  " 
+                        + item.getTotal() + "\n";
+        }
+
+        bill = bill + "--------------------------------\n";
+        
+        bill = bill + "Subtotal:        " + order.getSubtotal() + "\n";
+        bill = bill + "Tax (5%):        " + getTax() + "\n";
+        bill = bill + "FINAL TOTAL:     " + getTotal() + "\n";
+        bill = bill + "================================";
+
+        return bill;
+    }
+}
