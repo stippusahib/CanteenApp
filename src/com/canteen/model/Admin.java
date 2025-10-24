1e1caee6439df936234dfea1a4d5cab2d252b4b4
S TIPPU SAHIB <185490741+stippusahib@users.noreply.github.com>
2025-10-24 14:38:06 +0530

    Add Admin class with constructor and getters

--- src/com/canteen/model/Admin.java
+++ src/com/canteen/model/Admin.java
@@ -1,1 +1,19 @@
+package com.canteen.model;
 
+public class Admin {
+    private int adminId;
+    private String email;
+
+    public Admin(int adminId, String email) {
+        this.adminId = adminId;
+        this.email = email;
+    }
+
+    public int getAdminId() {
+        return adminId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
