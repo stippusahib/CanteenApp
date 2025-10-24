13c287879929a994e605650334832adf55688e00
Ann Mariya Tony <annmariyatony2028@cs.sjcetpalai.ac.in>
2025-10-24 12:26:50 +0530

    Update MenuItem.java

--- src/com/canteen/model/MenuItem.java
+++ src/com/canteen/model/MenuItem.java
@@ -1,1 +1,25 @@
-w
+package com.canteen.model;
+
+public class MenuItem {
+    private int itemId;
+    private String name;
+    private double price;
+
+    public MenuItem(int itemId, String name, double price) {
+        this.itemId = itemId;
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
