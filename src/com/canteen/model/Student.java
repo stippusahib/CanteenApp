d72b4b95242cd47010526eb98fb611799eac3c8f
Ann Mariya Tony <annmariyatony2028@cs.sjcetpalai.ac.in>
2025-10-24 12:41:35 +0530

    Update Student.java

--- src/com/canteen/model/Student.java
+++ src/com/canteen/model/Student.java
@@ -1,1 +1,19 @@
+package com.canteen.model;
 
+public class Student {
+    private int studentId;
+    private String email;
+
+    public Student(int studentId, String email) {
+        this.studentId = studentId;
+        this.email = email;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
